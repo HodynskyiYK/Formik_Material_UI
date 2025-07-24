8a6fb249ef12cd13258c868ccf97d1efc9cfff4a
y.hodynskyi <yurii.hodynskyi@rsli.com>
2025-07-24 14:45:25 +0300

    Configure pages structure

---
+++ src/pages/EditUserPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditUserPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Edit User</h1>
+    </div>
+  );
+};
+
+export default EditUserPage;
---
+++ src/pages/UserPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserPage: React.FC = () => {
+  return (
+    <div>
+      <h1>User Page</h1>
+      <p>Welcome to the user page!</p>
+    </div>
+  );
+};
+
+export default UserPage;
