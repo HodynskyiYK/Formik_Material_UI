e2f5dc11b50dbf55d117567132952d3cfcc83881
y.hodynskyi <yurii.hodynskyi@rsli.com>
2025-07-25 14:49:19 +0300

    Added functionality on UserPage

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,12 +1,61 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useAppActions} from '../hooks/useAppAction.ts'
+import {useParams} from 'react-router-dom'
+import {useAppSelector} from '../hooks/useAppSelector.ts'
+import {LoadingState} from '../types/common.type.ts'
+import {Loading} from '../components/common/Loading.tsx'
+import {Alert} from '../components/common/Alert.tsx'
+import {UserItem} from '../components/users/UserItem.tsx'
+import {Grid} from '@mui/material'
+import {UserForm} from '../components/forms/UserForm.tsx'
 
 const UserPage: React.FC = () => {
-  return (
-    <div>
-      <h1>User Page</h1>
-      <p>Welcome to the user page!</p>
-    </div>
-  );
+    const {id} = useParams()
+    const {userById, status, error} = useAppSelector(state => state.users)
+    const {fetchUserById} = useAppActions()
+    const isLoading = status === LoadingState.LOADING
+
+    useEffect(() => {
+        if (id) {
+            fetchUserById(Number(id))
+        }
+    }, []);
+
+    if (isLoading) {
+        return (
+            <Loading/>
+        )
+    }
+
+    if (error) {
+        return (
+            <Alert message={error} severity={'error'}/>
+        )
+    }
+
+    if (userById === null) {
+        return (
+            <Alert message={`User by ${id} not found`} severity={'info'}/>
+        )
+    }
+
+    return (
+        <>
+
+            <h1>User Page</h1>
+            <Grid
+                container
+                direction="row"
+                sx={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <UserItem user={userById}/>
+            </Grid>
+            <UserForm user={userById} />
+        </>
+    );
 };
 
 export default UserPage;
